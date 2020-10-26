f513449e15c965c52aef5d450c5acbb3de062683
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-26 19:38:43 +0530

    Added :  Array of Different Data Types

--- test/java/com/bridgelabz/generics/PrintArray.java
+++ test/java/com/bridgelabz/generics/PrintArray.java
@@ -1,33 +1,41 @@
 package com.bridgelabz.generics;
 
 public class PrintArray {
-	
-	public static void toPrint(int[] a) {
-		for(int i : a) {
-			System.out.print(i+ "\t");
+		// Integer Array
+		public static void toPrint(int[] a)
+		{
+			for(int i : a) 
+			{
+				System.out.print(i+ "\t");
+			}
+			System.out.println("\n");
 		}
-		System.out.println("\n");
-	}
-	public static void toPrint(double[] b) {
-		for(double i : b) {
-			System.out.print(i + "\t");
+		//Double Array
+		public static void toPrint(double[] b)
+		{
+			for(double i : b)
+			{
+				System.out.print(i + "\t");
+			}
+			System.out.println("\n");
 		}
-		System.out.println("\n");
-	}
-	public static void toPrint(char[] c) {
-		for(char i : c) {
-			System.out.print(i + "\t");
+		//Character Array
+		public static void toPrint(char[] c)
+		{
+			for(char i : c)
+			{
+				System.out.print(i + "\t");
+			}
+			System.out.println("\n");
 		}
-		System.out.println("\n");
-	}
-	public static void main(String[] args) {
-		int[] intArr = {1,2,3,4};
-		double[] doubleArr = {18.0,15.0,850.5,10.2};
-		char[] charArr = {'a','h','a','n'};
-		toPrint(intArr);
-		toPrint(doubleArr);
-		toPrint(charArr);
-		
-	}
+		public static void main(String[] args)
+		{
+			int[] intArr = {1,2,3,4};
+			double[] doubleArr = {18.0,15.0,850.5,10.2};
+			char[] charArr = {'a','h','a','n'};
+			toPrint(intArr);
+			toPrint(doubleArr);
+			toPrint(charArr);
+		}
 
 }
