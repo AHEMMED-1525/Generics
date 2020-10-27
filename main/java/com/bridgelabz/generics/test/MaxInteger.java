7d9faf4a39220fc88f6f9385efd51bc087523ed4
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-27 20:41:32 +0530

    Refractor : code by adding only one Generic method for all data Types

--- main/java/com/bridgelabz/generics/test/MaxInteger.java
+++ main/java/com/bridgelabz/generics/test/MaxInteger.java
@@ -1,18 +1,8 @@
 package com.bridgelabz.generics.test;
 
 public class MaxInteger<E extends Comparable<E>> {
-	E varX , varY, varZ;
 
-	public MaxInteger(E varX, E varY, E varZ) {
-	
-		this.varX = varX;
-		this.varY = varY;
-		this.varZ = varZ;
-	}
-	public E CheckMaximumValue()
-	{
-		return (E) MaxInteger.CheckMaximumValue(varX, varY, varZ);
-	}
+	// Creating one Generic Method for all data types
 	public static <E extends Comparable<E>> E CheckMaximumValue (E varX, E varY, E varZ) {
 		E maximum = varX;
 		if((varY).compareTo(maximum) > 0 ) {
