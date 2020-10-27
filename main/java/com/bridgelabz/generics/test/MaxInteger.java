c062a14067502ea03f2745e357b7b6c3850553ae
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-27 20:34:35 +0530

    Refractor : code adding generic Class

--- main/java/com/bridgelabz/generics/test/MaxInteger.java
+++ main/java/com/bridgelabz/generics/test/MaxInteger.java
@@ -1,40 +1,27 @@
 package com.bridgelabz.generics.test;
 
-public class MaxInteger {
-	public int MaximumIntegerPosition(Integer x, Integer y, Integer z) {
-		Integer maxIntValue = x;
-		if(y.compareTo(maxIntValue) > 0)  	             
-		{
-			maxIntValue = y;
-		}
-		if(z.compareTo(maxIntValue) > 0)  	               
-		{
-			maxIntValue = z;
-		}
-		
-			return maxIntValue;
+public class MaxInteger<E extends Comparable<E>> {
+	E varX , varY, varZ;
+
+	public MaxInteger(E varX, E varY, E varZ) {
+	
+		this.varX = varX;
+		this.varY = varY;
+		this.varZ = varZ;
 	}
-	public float maxFloatPosition(Float var1, Float var2 , Float var3) {
-		float maxFloatValue = var1;
-		if(var2.compareTo(maxFloatValue) > 0) 		
-		{
-			maxFloatValue = var2;
-		}
-		if(var3.compareTo(maxFloatValue) > 0) 		 
-		{
-			maxFloatValue = var3;
-		}
-		return maxFloatValue;
+	public E CheckMaximumValue()
+	{
+		return (E) MaxInteger.CheckMaximumValue(varX, varY, varZ);
 	}
-	public String MaxStringLengthPosition(String str1, String str2, String str3) {
-		String maxString = str1;
-		if(str2.compareTo(maxString) > 0) {
-			maxString = str2;
+	public static <E extends Comparable<E>> E CheckMaximumValue (E varX, E varY, E varZ) {
+		E maximum = varX;
+		if((varY).compareTo(maximum) > 0 ) {
+			maximum = varY;
 		}
-		if(str3.compareTo(maxString) > 0) {
-			maxString = str3;
+		if(varZ.compareTo(maximum) > 0 ) {
+			maximum = varZ;
 		}
-		return maxString;
+		return maximum ;
 	}
 	
 	
--- test/java/com/bridgelabz/generics/test/MaximumInteger.java
+++ test/java/com/bridgelabz/generics/test/MaximumInteger.java
@@ -8,64 +8,64 @@ public class MaximumInteger {
 	// Test case 1.1
 	@Test
 	public void givenInteger_WhenProper_ReturnsFirstMaximum() {
-		MaxInteger maxTest = new MaxInteger();
-		int result1 = maxTest.MaximumIntegerPosition(90, 40, 50);
+		MaxInteger maxTest = new MaxInteger(90, 40, 50);
+		int result1 = (Integer) maxTest.CheckMaximumValue();
 		Assert.assertEquals(90,result1);
 	}
 	// Test case 1.2
 	@Test
 	public void givenInteger_WhenProper_ReturnsSecondInteger() {
-		MaxInteger maxTest = new MaxInteger();
-		int result2 = maxTest.MaximumIntegerPosition(40, 80, 50);
+		MaxInteger maxTest = new MaxInteger(60, 80, 50);
+		int result2 = (Integer) maxTest.CheckMaximumValue();
 		Assert.assertEquals(80, result2);
 	}
 	// Test case 1.3
 	@Test
 	public void givenInteger_WhenProper_ReturnThirdMaximum() {
-		MaxInteger maxTest = new MaxInteger();
-		int result3 = maxTest.MaximumIntegerPosition(50, 60, 100);
+		MaxInteger maxTest = new MaxInteger(25, 50, 100);
+		int result3 = (Integer) maxTest.CheckMaximumValue();
 		Assert.assertEquals(100, result3);	
 	}
 	// test case 2.1
 	@Test
 	public void givenFloatValue_WhenProper_ReturnFirstMaximum() {
-		MaxInteger maxTest = new MaxInteger();
-		float FloatResult = maxTest.maxFloatPosition(15.214f, 8.541f, 10.54f);
+		MaxInteger maxTest = new MaxInteger(15.214f, 8.541f, 10.54f);
+		float FloatResult = (Float) maxTest.CheckMaximumValue();
 		Assert.assertEquals(15.214, FloatResult,15.214f);
 	}
 	// Test case 2.2
 	@Test
 	public void givenFloatValue_WhenProper_ReturnSecondMaximum() {
-		MaxInteger maxTest = new MaxInteger();
-		float FloatResult = maxTest.maxFloatPosition(15.214f, 18.541f, 10.54f);
+		MaxInteger maxTest = new MaxInteger(15.214f, 18.541f, 10.54f);
+		float FloatResult = (Float) maxTest.CheckMaximumValue();
 		Assert.assertEquals(18.514f, FloatResult,18.54f);
 	}
 	// Test case 2.3
 	@Test
 	public void givenFloatValue_WhenProper_ReturnThirdMaximum() {
-		MaxInteger maxTest = new MaxInteger();
-		float FloatResult = maxTest.maxFloatPosition(15.214f, 18.541f, 20.54f);
+		MaxInteger maxTest = new MaxInteger(15.214f, 18.541f, 20.54f);
+		float FloatResult = (Float) maxTest.CheckMaximumValue();
 		Assert.assertEquals(20.54f, FloatResult,20.54f);
 	}
 	// test case 3.1
 	@Test
 	public void givenString_WhenProper_RetutnFirstMax() {
-		MaxInteger maxTest = new MaxInteger();
-		String StringResult = maxTest.MaxStringLengthPosition("on", "Lucky", "Food");
+		MaxInteger maxTest = new MaxInteger("on", "Lucky", "Food");
+		String StringResult = (String) maxTest.CheckMaximumValue();
 		Assert.assertEquals("on", StringResult);
 	}
 	// test case 3.2
 	@Test
 	public void givenString_WhenProper_RetutnSecondMax() {
-		MaxInteger maxTest = new MaxInteger();
-		String StringResult = maxTest.MaxStringLengthPosition("Colour", "sky", "Blue");
+		MaxInteger maxTest = new MaxInteger("Colour", "sky", "Blue");
+		String StringResult = (String) maxTest.CheckMaximumValue();
 		Assert.assertEquals("sky", StringResult);
 	}
 	// test case 3.3
 	@Test
 	public void givenString_WhenProper_RetutnThirdMax() {
-		MaxInteger maxTest = new MaxInteger();
-		String StringResult = maxTest.MaxStringLengthPosition("Forest", "Kings", "Lion");
+		MaxInteger maxTest = new MaxInteger("Forest", "Kings", "Lion");
+		String StringResult = (String) maxTest.CheckMaximumValue();
 		Assert.assertEquals("Lion", StringResult);
 	}
 
