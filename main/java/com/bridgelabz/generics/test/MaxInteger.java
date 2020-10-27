e31658a17fdc414c3a62af2b2123ca991cb83e85
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-27 19:24:49 +0530

    Added : code to find maximum of different data types using TestcASES

--- main/java/com/bridgelabz/generics/test/MaxInteger.java
+++ main/java/com/bridgelabz/generics/test/MaxInteger.java
@@ -1,57 +1,40 @@
 package com.bridgelabz.generics.test;
 
 public class MaxInteger {
-	public boolean MaxAtFirstPosition(Integer x, Integer y, Integer z) {
-		if(x.compareTo(y) > 0 && x.compareTo(z) > 0)  	               // If true return First position maximum
+	public int MaximumIntegerPosition(Integer x, Integer y, Integer z) {
+		Integer maxIntValue = x;
+		if(y.compareTo(maxIntValue) > 0)  	             
 		{
-			System.out.println("First Maximum Integer is : " +x);
-			return true;
+			maxIntValue = y;
 		}
-		else if(y.compareTo(x) > 0 && y.compareTo(z) > 0)	           //if true return Second Position maximum 
+		if(z.compareTo(maxIntValue) > 0)  	               
 		{
-			System.out.println("Second Maximum Integer is : " +y);
-			return true;
-		} 
-		else if(z.compareTo(x) > 0 && z.compareTo(y) > 0)          	  // if true returns Third Position Maximum
-		{
-			System.out.println("Third Maximum Integer is :" +z );
-			return true;
+			maxIntValue = z;
 		}
-		else
-			return false;
+		
+			return maxIntValue;
 	}
-	public boolean maxFloatValuePosition(Float var1, Float var2 , Float var3) {
-		if(var1.compareTo(var2) > 0 && var1.compareTo(var3) > 0)             // If true return First position maximum
+	public float maxFloatPosition(Float var1, Float var2 , Float var3) {
+		float maxFloatValue = var1;
+		if(var2.compareTo(maxFloatValue) > 0) 		
 		{
-			System.out.println("Maximum First Float value is : " +var1);
-			return true;
+			maxFloatValue = var2;
 		}
-		else if(var2.compareTo(var1) > 0 && var2.compareTo(var3) > 0) 		//if true return Second Position maximum 
+		if(var3.compareTo(maxFloatValue) > 0) 		 
 		{
-			System.out.println("maximum Second Float is : " +var2);
-			return true;
+			maxFloatValue = var3;
 		}
-		else if(var3.compareTo(var1) > 0 && var3.compareTo(var2) > 0) 		 // if true returns Third Position Maximum
-		{
-			System.out.println("Maximum Third Float value is : " +var3);
-			return true;
-		}
-		return false;
+		return maxFloatValue;
 	}
-	public boolean MaxStringLengthPosition(String str1, String str2, String str3) {
-		if(str1.length() > str2.length() && str1.length() > str2.length()) {
-			System.out.println("First Maximum Length of the String is : " + str1);
-			return true;
+	public String MaxStringLengthPosition(String str1, String str2, String str3) {
+		String maxString = str1;
+		if(str2.compareTo(maxString) > 0) {
+			maxString = str2;
 		}
-		if(str2.length() > str1.length() && str2.length() > str3.length()) {
-			System.out.println("Second Maximum Length of the String is : " + str2);
-			return true;
+		if(str3.compareTo(maxString) > 0) {
+			maxString = str3;
 		}
-		if(str3.length() > str1.length() && str3.length() > str2.length()) {
-			System.out.println("Third Maximum Length of the String is : " + str3);
-			return true;
-		}
-		return false;
+		return maxString;
 	}
 	
 	
--- test/java/com/bridgelabz/generics/test/MaximumInteger.java
+++ test/java/com/bridgelabz/generics/test/MaximumInteger.java
@@ -9,66 +9,64 @@ public class MaximumInteger {
 	@Test
 	public void givenInteger_WhenProper_ReturnsFirstMaximum() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean result1 = maxTest.MaxAtFirstPosition(90, 40, 50);
-		Assert.assertEquals(true, result1);
+		int result1 = maxTest.MaximumIntegerPosition(90, 40, 50);
+		Assert.assertEquals(90,result1);
 	}
 	// Test case 1.2
 	@Test
 	public void givenInteger_WhenProper_ReturnsSecondInteger() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean result2 = maxTest.MaxAtFirstPosition(40, 80, 50);
-		Assert.assertEquals(true, result2);
+		int result2 = maxTest.MaximumIntegerPosition(40, 80, 50);
+		Assert.assertEquals(80, result2);
 	}
 	// Test case 1.3
 	@Test
 	public void givenInteger_WhenProper_ReturnThirdMaximum() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean result3 = maxTest.MaxAtFirstPosition(50, 60, 100);
-		Assert.assertEquals(true, result3);	
+		int result3 = maxTest.MaximumIntegerPosition(50, 60, 100);
+		Assert.assertEquals(100, result3);	
 	}
-	// User cases 2 maximum Float value
 	// test case 2.1
 	@Test
 	public void givenFloatValue_WhenProper_ReturnFirstMaximum() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean FloatResult = maxTest.maxFloatValuePosition(15.214f, 8.541f, 10.54f);
-		Assert.assertEquals(true, FloatResult);
+		float FloatResult = maxTest.maxFloatPosition(15.214f, 8.541f, 10.54f);
+		Assert.assertEquals(15.214, FloatResult,15.214f);
 	}
 	// Test case 2.2
 	@Test
 	public void givenFloatValue_WhenProper_ReturnSecondMaximum() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean FloatResult = maxTest.maxFloatValuePosition(15.214f, 18.541f, 10.54f);
-		Assert.assertEquals(true, FloatResult);
+		float FloatResult = maxTest.maxFloatPosition(15.214f, 18.541f, 10.54f);
+		Assert.assertEquals(18.514f, FloatResult,18.54f);
 	}
 	// Test case 2.3
 	@Test
 	public void givenFloatValue_WhenProper_ReturnThirdMaximum() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean FloatResult = maxTest.maxFloatValuePosition(15.214f, 18.541f, 20.54f);
-		Assert.assertEquals(true, FloatResult);
+		float FloatResult = maxTest.maxFloatPosition(15.214f, 18.541f, 20.54f);
+		Assert.assertEquals(20.54f, FloatResult,20.54f);
 	}
-	// test case 3
 	// test case 3.1
 	@Test
 	public void givenString_WhenProper_RetutnFirstMax() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean StringResult = maxTest.MaxStringLengthPosition("Ahemmed", "Lucky", "Food");
-		Assert.assertEquals(true, StringResult);
+		String StringResult = maxTest.MaxStringLengthPosition("on", "Lucky", "Food");
+		Assert.assertEquals("on", StringResult);
 	}
 	// test case 3.2
 	@Test
 	public void givenString_WhenProper_RetutnSecondMax() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean StringResult = maxTest.MaxStringLengthPosition("sky", "Colour", "Blue");
-		Assert.assertEquals(true, StringResult);
+		String StringResult = maxTest.MaxStringLengthPosition("Colour", "sky", "Blue");
+		Assert.assertEquals("sky", StringResult);
 	}
 	// test case 3.3
 	@Test
 	public void givenString_WhenProper_RetutnThirdMax() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean StringResult = maxTest.MaxStringLengthPosition("Lion", "King", "of Forest");
-		Assert.assertEquals(true, StringResult);
+		String StringResult = maxTest.MaxStringLengthPosition("Forest", "Kings", "Lion");
+		Assert.assertEquals("Lion", StringResult);
 	}
 
 }
