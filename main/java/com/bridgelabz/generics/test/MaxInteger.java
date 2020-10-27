17ecf5eaa262ce90178bf678411e88bc37a53281
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-27 11:10:13 +0530

    Added : Code to find Maximum float value

--- main/java/com/bridgelabz/generics/test/MaxInteger.java
+++ main/java/com/bridgelabz/generics/test/MaxInteger.java
@@ -1,33 +1,42 @@
 package com.bridgelabz.generics.test;
 
 public class MaxInteger {
-	// First position maximum
 	public boolean MaxAtFirstPosition(Integer x, Integer y, Integer z) {
-		if(x.compareTo(y) > 0 && x.compareTo(z) > 0) 
+		if(x.compareTo(y) > 0 && x.compareTo(z) > 0)  	               // If true return First position maximum
 		{
 			System.out.println("First Maximum Integer is : " +x);
 			return true;
 		}
-		else
-			return false;
-		}
-	// Second Position maximun
-	public boolean MaxAtSecondPosition(Integer x, Integer y, Integer z) {
-		if(y.compareTo(x) > 0 && y.compareTo(z) > 0) {
+		else if(y.compareTo(x) > 0 && y.compareTo(z) > 0)	           //if true return Second Position maximum 
+		{
 			System.out.println("Second Maximum Integer is : " +y);
 			return true;
-		}
-		else
-			return false;
-	}
-	// Third Position Maximum
-	public boolean MaxAtThirdPosition(Integer x, Integer y, Integer z) {
-		if(z.compareTo(x) > 0 && z.compareTo(y) > 0) {
+		} 
+		else if(z.compareTo(x) > 0 && z.compareTo(y) > 0)          	  // if true returns Third Position Maximum
+		{
 			System.out.println("Third Maximum Integer is :" +z );
 			return true;
 		}
 		else
 			return false;
+	}
+	public boolean maxFloatValuePosition(Float var1, Float var2 , Float var3) {
+		if(var1.compareTo(var2) > 0 && var1.compareTo(var3) > 0)             // If true return First position maximum
+		{
+			System.out.println("Maximum First Float value is : " +var1);
+			return true;
+		}
+		else if(var2.compareTo(var1) > 0 && var2.compareTo(var3) > 0) 		//if true return Second Position maximum 
+		{
+			System.out.println("maximum Second Float is : " +var2);
+			return true;
+		}
+		else if(var3.compareTo(var1) > 0 && var3.compareTo(var2) > 0) 		 // if true returns Third Position Maximum
+		{
+			System.out.println("Maximum Third Float value is : " +var3);
+			return true;
+		}
+		return false;
 	}
 	
 	
--- test/java/com/bridgelabz/generics/test/MaximumInteger.java
+++ test/java/com/bridgelabz/generics/test/MaximumInteger.java
@@ -16,16 +16,40 @@ public class MaximumInteger {
 	@Test
 	public void givenInteger_WhenProper_ReturnsSecondInteger() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean result2 = maxTest.MaxAtSecondPosition(40, 80, 50);
+		boolean result2 = maxTest.MaxAtFirstPosition(40, 80, 50);
 		Assert.assertEquals(true, result2);
 	}
 	// Test case 1.3
 	@Test
 	public void givenInteger_WhenProper_ReturnThirdMaximum() {
 		MaxInteger maxTest = new MaxInteger();
-		boolean result3 = maxTest.MaxAtThirdPosition(50, 60, 100);
+		boolean result3 = maxTest.MaxAtFirstPosition(50, 60, 100);
 		Assert.assertEquals(true, result3);
+		System.out.println();
 		
+	}
+	
+	// User cases 2 maximum Float value
+	// test case 2.1
+	@Test
+	public void givenFloatValue_WhenProper_ReturnFirstMaximum() {
+		MaxInteger maxTest = new MaxInteger();
+		boolean FloatResult = maxTest.maxFloatValuePosition(15.214f, 8.541f, 10.54f);
+		Assert.assertEquals(true, FloatResult);
+	}
+	// Test case 2.2
+	@Test
+	public void givenFloatValue_WhenProper_ReturnSecondMaximum() {
+		MaxInteger maxTest = new MaxInteger();
+		boolean FloatResult = maxTest.maxFloatValuePosition(15.214f, 18.541f, 10.54f);
+		Assert.assertEquals(true, FloatResult);
+	}
+	// Test case 2.3
+	@Test
+	public void givenFloatValue_WhenProper_ReturnThirdMaximum() {
+		MaxInteger maxTest = new MaxInteger();
+		boolean FloatResult = maxTest.maxFloatValuePosition(15.214f, 18.541f, 20.54f);
+		Assert.assertEquals(true, FloatResult);
 	}
 
 }
